0d734aa3ac9d180f309e3e2bfce4221949f6b4b4
jenpowroznyk <35268767+jenpowroznyk@users.noreply.github.com>
2019-10-30 00:57:59 -0400

    Char Anim, Controller, Puzzle
    - Walk animation on character is only active when using "W"
    (Something to get with "GetAxis" value and Blend Tree)
    - Grass has completely stopped working
    - Char controller is functional but gravity is not strong enough

---
+++ Blush/Assets/BunProject/Scripts/BlendTreeScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendTreeScript : MonoBehaviour
+{
+    private Animator anim;
+    private float h;
+    private float v;
+
+    public KeyCode A;
+    public KeyCode B;
+    public KeyCode C;
+    public KeyCode D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        anim.SetFloat("Walk", v);
+        anim.SetFloat("Turn", h);
+    }
+}
+
+
